1fa09af74efa4eb5b3991ef72d3cc40554c4a24e
divya148284 <divya.pathak@capgemini.com>
2019-08-02 00:54:22 +0530

    SimpleInterest

---
+++ src/main/java/com/assignment/code/SimpleInterest.java
@@ -0,0 +1,33 @@
+package com.assignment.code;
+
+import java.util.Scanner;
+
+public class SimpleInterest{
+
+  
+    public static void main(String args[]) {
+      
+        
+        Scanner scanner = new Scanner(System.in);
+        System.err.println("Please enter principle amount :");
+        float amount = scanner.nextFloat();
+      
+        System.err.println("Enter time in years : ");
+        float time = scanner.nextFloat();
+      
+        System.out.println("Enter rate annually : ");
+        float rate = scanner.nextFloat();
+      
+        float interest = simpleInterest(amount, rate, time);
+      
+        System.out.println("Simple interested calculate by program is : " + interest);
+    }
+  
+    public static  float simpleInterest(float principle, float rate, float time){
+        float interest = (principle*rate*time)/100;
+        return interest;
+    }
+    
+    
+}
+
---
+++ src/test/java/com/assignment/test/SimpleInterestTest.java
@@ -0,0 +1,44 @@
+package com.assignment.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.assignment.code.SimpleInterest;
+
+//import com. assignment.code.Armstrong;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleInterestTest {
+
+	SimpleInterest sim;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("before class");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSimpleInterestValue() {
+		System.out.println("test case find max");
+
+		Assert.assertEquals(sim.simpleInterest(1000, 10, 30), 3000, 0.001);
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+}
